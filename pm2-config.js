9b7904fdcc84d0c409253d3705b94b472e650953
rsbertoa90 <rsbertoa90@gmail.com>
2019-05-27 17:40:59 -0300

    config pm2

---
+++ pm2-config.js
@@ -0,0 +1,18 @@
+  {
+    "apps": [{
+      // Portfolio Application
+      "name": "matesdefabrica.com",
+      "script": "./node_modules/nuxt/bin/nuxt-start",
+      "args": [],
+      "node_args": "",
+      "merge_logs": true,
+      "cwd": "/home/forge/matesdefabrica.com/",
+      "env": {
+        "PORT": "3000"
+      },
+      "env_production": {
+        "NODE_ENV": "production",
+        "PORT": "3000"
+      }
+    }]
+  }

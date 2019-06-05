c62b1a4e6d30c64d1a13e91cfe6bf9ea55f4dfce
rsbertoa90 <rsbertoa90@gmail.com>
2019-06-05 16:09:02 -0300

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'matesdefabrica.com',
+    name: 'redlimp.com',
     script: './node_modules/nuxt/bin/nuxt.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/

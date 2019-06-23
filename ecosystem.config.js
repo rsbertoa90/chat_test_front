e9dc6b8558d53a0cdba5d02454e221f8707e98c7
rsbertoa90 <rsbertoa90@gmail.com>
2019-06-23 19:37:18 -0300

    cluster 4 instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
    /*  args: 'one two', */
-    instances: "max",
+    instances: 4,
     exec_mode: "cluster",
     autorestart: true,
     watch: false,

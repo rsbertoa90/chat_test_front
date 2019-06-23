a28c3adede0fa7c0314f22ececb4ed9d46cf5627
rsbertoa90 <rsbertoa90@gmail.com>
2019-06-23 19:43:51 -0300

    no cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
    /*  args: 'one two', */
-    instances: 4,
-    exec_mode: "cluster",
+    instances: 1,
+    //exec_mode: "cluster",
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',

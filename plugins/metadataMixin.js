051b0769c5112dca8461193149a3fc4106e1b8c1
rsbertoa90 <rsbertoa90@gmail.com>
2019-10-24 15:50:44 -0300

    sdata

--- plugins/metadataMixin.js
+++ plugins/metadataMixin.js
@@ -11,11 +11,45 @@ export default {
                 { name: 'description', content: this.metadescription,hid:'description' },
                 { property:'og:url', content:canonical}
 
-            ]
+            ],
+            __dangerouslyDisableSanitizers: ['script'],
+            script: [
+                {
+                    innerHTML: JSON.stringify(this.orgsd),
+                    type: 'application/ld+json'
+                },
+                {
+                    innerHTML: JSON.stringify(this.websd),
+                    type: 'application/ld+json'
+                }
+            ],
         }
     },
     computed: {
-       
+        websd() {
+            return {
+                "@context": "http://schema.org",
+                "@type": "WebPage",
+                "@id": "https://matesfabi.com",
+                "url": "https://matesfabi.com",
+                "name": "Mates Fabi"
+            }
+        },
+        orgsd() {
+            return {
+                "@context": "http://schema.org/",
+                "@type": "Organization",
+                "name": "Mates Fabi",
+                "url": "https://matesfabi.com",
+                "address": "Pasteur 410 - CABA",
+                "email": "MatesFabi@gmail.com",
+                "logo": 'https://back.matesfabi.com/storage/images/app/logo.png',
+                "telephone": "1130085414",
+                "description": "Mates, Equipos Mates , Latas, Juegos de Azucarera/Yerbera y mucho mas en nuestros Locales Mayoristas ubicados en Once. También regalos empresariales personalizados."
+
+
+            }
+        },
       
         metadata() {
             let page = this.$route.name;

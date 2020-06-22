231837286ce79f37b1162dfbdab3ac4dd726bd5a
Juan Pablo Aveggio <juan@pop-os.localdomain>
2020-06-22 13:23:00 -0300

    Se agrega el paquete nuxt-socket-io. Se agrega a nuxt.config.js el modulo y una configuracion de ejemplo comentada.

--- nuxt.config.js
+++ nuxt.config.js
@@ -164,7 +164,8 @@ export default {
     ],
     // Doc: https://axios.nuxtjs.org/usage
     "@nuxtjs/axios",
-    "@nuxtjs/auth"
+    "@nuxtjs/auth",
+    "nuxt-socket-io"
   ],
 
   'google-gtag': {
@@ -246,6 +247,27 @@ export default {
         // tokenType: 'bearer',
       }
     }
+  },
+
+  io: {
+    /* 
+    configuracion de nuxt-socket-io
+    sockets: [ // Required
+      { // At least one entry is required
+        name: 'home',
+        url: 'http://localhost:3000',
+        default: true,
+        vuex: { // see section below  
+        },
+        namespaces: { // see section below 
+        }
+      }, 
+      { name: 'work', url: 'http://somedomain1:3000' },
+      { name: 'car', url: 'http://somedomain2:3000' },
+      { name: 'tv', url: 'http://somedomain3:3000' },
+      { name: 'test', url: 'http://localhost:4000' }
+    ]
+    */
   },
 
   /*

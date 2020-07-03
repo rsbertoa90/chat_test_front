a6f92cc4255deea9457e1a78bde0cbc3aa552c51
Juan Pablo Aveggio <juan@pop-os.localdomain>
2020-07-03 15:51:19 -0300

    clean

--- components/chat/footer.vue
+++ components/chat/footer.vue
@@ -3,7 +3,7 @@
         <attachment 
             :file="file"
             @cancelAttach="$refs.form.resetFile()"
-            @isATicketChange="isATicket = $event" />
+            @isATicketChange="isATicket=$event" />
 
         <chat-form ref="form"
             :conversation="conversation"
@@ -11,34 +11,7 @@
             @fileChange="file=$event" 
             @writingChange="$emit('writingChange', $event)" 
             @sendMessage="$emit('sendMessage', $event)" />
-
-<!--
-        <form
-            v-if="conversation"
-            @submit.prevent="send"
-            class="d-flex align-items-center message-form"
-        >
-            <input
-                v-model="newMessage"
-                type="text"
-                class="form-control mat material-shadow-1"
-            />
-            <label class="adj-btn d-flex">
-                <i class="material-icons">attach_file</i>
-                <input
-                    @change="onFileChange"
-                    type="file"
-                    name="file"
-                    accept="image/x-png, image/gif, image/jpeg"
-                    class="display-none"
-                    ref="fileInput"
-                />
-            </label>
-            <button type="submit" :disabled="isSendDisabled" class="d-flex mat">
-                <i class="material-icons">send</i>
-            </button>
-        </form>
-        -->
+        
         <bottom-panel />
     </div>
 </template>
@@ -53,53 +26,9 @@ export default {
     data() {
         return {
             isATicket: true,
-            newMessage: "",
-            imWriting: false,
-//            imageUploaded: false,
             file: null
         };
     },
-    computed: {
-
-    },
-    methods: {
-        /*
-        onSendMessage() {
-            var fdata = new FormData();
-            var shouldSend = false;
-
-            if (this.file) {
-                shouldSend = true;
-                fdata.append("image", file);
-            }
-
-            if (this.newMessage.trim()) {
-                shouldSend = true;
-                fdata.append("content", this.newMessage);
-            }
-
-            if (shouldSend) {
-                fdata.append("conversation_id", this.conversation.id);
-                fdata.append("prio_auto", this.isATicket ? 1 : 0);
-                this.$emit('sendMessage', {fdata: fdata, isATicket: this.isATicket} );
-            }
-            this.newMessage = "";
-            this.resetFileInput();
-        },*/
-    },
-    watch: {
-        /*
-        newMessage() {
-            if (
-                (this.newMessage.trim() && !this.imWriting) ||
-                (!this.newMessage.trim() && this.imWriting)
-            ) {
-                this.imWriting = !this.imWriting;
-                this.$emit('writingChange', this.imWriting);
-            }
-        }
-        */
-    }
 };
 
 </script>

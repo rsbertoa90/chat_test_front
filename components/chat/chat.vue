f4df487ebeacab9db79eadbf996458cb29006926
Juan Pablo Aveggio <juan@pop-os.localdomain>
2020-07-03 12:40:27 -0300

    step three

--- components/chat/chat.vue
+++ components/chat/chat.vue
@@ -1,66 +1,20 @@
 <template>
     <div class="d-flex flex-column chat">
         <chatHeader v-if="conversation" :conversation="conversation" />
-        <div v-if="conversation" class="d-flex flex-column flex-grow-1" @mouseover="iSawTheMessages()">
-            <conversation :conversation="conversation" ref="conversation" />
+        <div
+            v-if="conversation"
+            class="d-flex flex-column flex-grow-1"
+            @mouseover="iSawTheMessages()"
+        >
+            <conversation 
+                :conversation="conversation" 
+                ref="conversation"/>
+           
+            <chat-footer
+                :conversation="conversation"
+                @writingChange="onWritingChange"
+                @sendMessage="onSendMessage"/>
 
-            <div v-if="conversation" class="form-container d-flex flex-column">
-                <div class="row w-100 d-flex justify-content-between pl-4 mb-3" v-if="preview">
-                    <div class="col-4">
-                        <div class="preview-img">
-                            <img :src="preview" v-if="preview" />
-                        </div>
-                    </div>
-                    <div class="col-7 fcc" v-if="!admin">
-                        <div
-                            class="d-flex justify-content-center text-center mt-2 isaticket-q"
-                        >¿ ESTAS ADJUNTANDO UN COMPROBANTE DE PAGO ?</div>
-                        <div class="d-flex justify-content-around mt-2">
-                            <button
-                                class="btn btn-lg isaticket-a"
-                                @click="isATicket=true"
-                                :class="{'btn-success':isATicket,'btn-secondary':!isATicket}"
-                            >SI</button>
-                            <button
-                                class="btn btn-lg isaticket-a"
-                                @click="isATicket=false"
-                                :class="{'btn-danger':!isATicket,'btn-secondary':isATicket}"
-                            >NO</button>
-                        </div>
-                    </div>
-                    <div class="col-1 p-0 d-flex flex-column">
-                        <button class="mat m-1" @click="resetFileInput()">
-                            <i class="material-icons">close</i>
-                        </button>
-                    </div>
-                </div>
-                <form
-                    v-if="conversation"
-                    @submit.prevent="sendMessage"
-                    class="d-flex align-items-center message-form"
-                >
-                    <input
-                        v-model="newMessage"
-                        type="text"
-                        class="form-control mat material-shadow-1"
-                        @keypress="iSawTheMessages()"
-                    />
-                    <label class="adj-btn d-flex">
-                        <i class="material-icons">attach_file</i>
-                        <input
-                            @change="onFileChange"
-                            type="file"
-                            name="file"
-                            accept="image/x-png, image/gif, image/jpeg"
-                            class="display-none"
-                            ref="fileInput"
-                        />
-                    </label>
-                    <button type="submit" :disabled="isSendDisabled" class="d-flex mat">
-                        <i class="material-icons">send</i>
-                    </button>
-                </form>
-            </div>
         </div>
         <div v-else class="d-flex justify-content-center align-items-center flex-grow-1">
             <span class="d-flex">Seleccione una conversación.</span>
@@ -69,25 +23,21 @@
 </template>
 
 <script>
-import chatHeader from './header.vue';
-import conversation from './conversation.vue';
-import chatFooter from './footer.vue';
+import chatHeader from "./header.vue";
+import conversation from "./conversation.vue";
+import chatFooter from "./footer.vue";
 
 export default {
     components: { chatHeader, conversation, chatFooter },
     data() {
         return {
-            isATicket: true,
-            newMessage: "",
-            imWriting: false,
             hesWriting: false,
-
             imOnline: true,
             hesOnline: false,
-
+/*
             imageUploaded: false,
-            file: null,
-            preview: null
+            file: null
+            */
         };
     },
     mounted() {
@@ -141,26 +91,10 @@ export default {
         },
         conversations() {
             return this.$store.getters.getConversations;
-        },
-        isSendDisabled() {
-            return !this.newMessage.trim().length && !this.preview;
         }
     },
     watch: {
-        newMessage() {
-            if (
-                (this.newMessage.trim() && !this.imWriting) ||
-                (!this.newMessage.trim() && this.imWriting)
-            ) {
-                this.imWriting = !this.imWriting;
-                let data = {
-                    conversation_id: this.conversation.id,
-                    writing: this.imWriting,
-                    user_id: this.user.id
-                };
-                this.socket.emit("imWriting", data);
-            }
-        },
+
         conversation(n, o) {
             if (this.conversation) {
                 /* conecto a la sala */
@@ -177,20 +111,9 @@ export default {
         }
     },
     methods: {
-        onFileChange(e) {
-            const file = e.target.files[0];
-            if (file) {
-                this.preview = URL.createObjectURL(file);
-                this.imageUploaded = true;
-            }
-        },
-        resetFileInput() {
-            this.$refs.fileInput.value = "";
-            this.preview = null;
-            this.imageUploaded = false;
-        },
+
         iSawTheMessages() {
-            console.log("isSawTheMessages");
+            // console.log("isSawTheMessages");
             if (this.conversation.unreads) {
                 var vm = this;
                 this.$axios.put(`/view-messages/${this.conversation.id}`);
@@ -217,54 +140,35 @@ export default {
             };
             this.socket.emit("sendNewMessage", data);
         },
-
-        sendMessage() {
+        onSendMessage(event) {
+            /*
+            console.log("sendMessage");
+            console.log(event.fdata);
+            console.log("isATicket " + event.isATicket);
+            */
+            
             var vm = this;
-            var fdata = new FormData();
-            var shouldSend = false;
+            this.$axios.post("/message", event.fdata).then(r => {
+                vm.socketMessage(r.data);
+                vm.$store.commit("addMessageToActiveConversation", r.data);
+                vm.$refs.conversation.scrollToBottom();
 
-            if (this.preview) {
-                var file = $('input[type="file"]')[0].files[0];
-                if (file) {
-                    shouldSend = true;
-                    fdata.append("image", file);
+                if (this.admin) {
+                    let d = {
+                        field: "last_message",
+                        value: r.data,
+                        conversation_id: this.conversation.id
+                    };
+                    this.$store.commit("updateConversation", d);
+                    this.$store.commit(
+                        "relocateConversation",
+                        this.conversation
+                    );
                 }
-            }
+            });
 
-            if (this.newMessage.trim()) {
-                shouldSend = true;
-                fdata.append("content", this.newMessage);
-            }
-
-            if (shouldSend) {
-                fdata.append("conversation_id", this.conversation.id);
-                fdata.append("prio_auto", this.isATicket ? 1 : 0);
-               
-               this.$axios.post("/message", fdata).then(r => {
-                    vm.socketMessage(r.data);
-                    vm.$store.commit("addMessageToActiveConversation", r.data);
-                    vm.$refs.conversation.scrollToBottom();
-
-                    if (this.admin) {
-                        let d = {
-                            field: "last_message",
-                            value: r.data,
-                            conversation_id: this.conversation.id
-                        };
-                        this.$store.commit("updateConversation", d);
-                        this.$store.commit(
-                            "relocateConversation",
-                            this.conversation
-                        );
-                    }
-                });
-            }
-
-            this.newMessage = "";
-            this.resetFileInput();
-
-            if (this.isATicket) {
-                let data = {
+            if (event.isATicket) {
+                const data = {
                     conversation_id: this.conversation.id,
                     field: "prio_auto",
                     value: true
@@ -275,14 +179,21 @@ export default {
                 this.socket.emit("updateConversation", data);
             }
         },
-
+        onWritingChange(writing) {
+            console.log("onWritingChange");
+            const data = {
+                conversation_id: this.conversation.id,
+                writing: writing,
+                user_id: this.user.id
+            };
+            this.socket.emit("imWriting", data);
+            this.iSawTheMessages();
+        }
     }
 };
-
 </script>
 
 <style lang="scss" scoped>
-
 .miniature-img {
     cursor: pointer;
     width: 200px;
@@ -310,62 +221,5 @@ export default {
 }
 .empty {
     box-sizing: border-box;
-}
-.form-container {
-    background: #e4e4e4;
-    border-top: solid 1px #e6e6e6;
-    outline: none;
-    z-index: 2;
-    box-shadow: 0 -2px 4px 0px rgba(0, 0, 0, 0.1);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-.message-form {
-    padding: 0 8px;
-    width: 100%;
-    align-items: center;
-}
-.message-form input {
-    margin: 8px 0;
-    flex-grow: 1;
-    // height: px;
-    border-radius: 19px;
-    border: 0;
-    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.25);
-}
-.message-form input:focus {
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.25);
-}
-
-.message-form button,
-.adj-btn {
-    border: none;
-    background: transparent;
-    padding: 0;
-    outline: none;
-    // height: 38px;
-    color: #6fcea3;
-    margin: 0;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
-    transition: box-shadow 0.15s ease-in-out;
-    cursor: pointer;
-}
-.message-form button:disabled {
-    color: #cccccc !important;
-}
-.message-form button:hover,
-.adj-btn {
-    color: #5db68e;
-}
-.message-form button:active {
-    color: #36a774;
-    text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.25);
-}
-
-.message-form button i,
-.adj-btn i {
-    font-size: 32px;
 }
 </style>
--- components/chat/footer.vue
+++ components/chat/footer.vue
@@ -1,13 +1,191 @@
 <template>
-    
+    <div v-if="conversation" class="form-container d-flex flex-column">
+        <div class="row w-100 d-flex justify-content-between pl-4 mb-3" v-if="preview">
+            <div class="col-4">
+                <div class="preview-img">
+                    <img :src="preview" v-if="preview" />
+                </div>
+            </div>
+            <div class="col-7 fcc" v-if="!admin">
+                <div
+                    class="d-flex justify-content-center text-center mt-2 isaticket-q"
+                >¿ ESTAS ADJUNTANDO UN COMPROBANTE DE PAGO ?</div>
+                <div class="d-flex justify-content-around mt-2">
+                    <button
+                        class="btn btn-lg isaticket-a"
+                        @click="isATicket=true"
+                        :class="{'btn-success':isATicket,'btn-secondary':!isATicket}"
+                    >SI</button>
+                    <button
+                        class="btn btn-lg isaticket-a"
+                        @click="isATicket=false"
+                        :class="{'btn-danger':!isATicket,'btn-secondary':isATicket}"
+                    >NO</button>
+                </div>
+            </div>
+            <div class="col-1 p-0 d-flex flex-column">
+                <button class="mat m-1" @click="resetFileInput()">
+                    <i class="material-icons">close</i>
+                </button>
+            </div>
+        </div>
+        <form
+            v-if="conversation"
+            @submit.prevent="send"
+            class="d-flex align-items-center message-form"
+        >
+            <input
+                v-model="newMessage"
+                type="text"
+                class="form-control mat material-shadow-1"
+            />
+            <label class="adj-btn d-flex">
+                <i class="material-icons">attach_file</i>
+                <input
+                    @change="onFileChange"
+                    type="file"
+                    name="file"
+                    accept="image/x-png, image/gif, image/jpeg"
+                    class="display-none"
+                    ref="fileInput"
+                />
+            </label>
+            <button type="submit" :disabled="isSendDisabled" class="d-flex mat">
+                <i class="material-icons">send</i>
+            </button>
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
-    
-}
+    props: ["conversation"],
+    data() {
+        return {
+            preview: null,
+            isATicket: true,
+            newMessage: "",
+            imWriting: false,
+            imageUploaded: false,
+            file: null
+        };
+    },
+    computed: {
+        isSendDisabled() {
+            return !this.newMessage.trim().length && !this.preview;
+        }
+    },
+    methods: {
+        onFileChange(e) {
+            const file = e.target.files[0];
+            if (file) {
+                this.preview = URL.createObjectURL(file);
+                this.imageUploaded = true;
+            }
+        },
+        resetFileInput() {
+            this.$refs.fileInput.value = "";
+            this.preview = null;
+            this.imageUploaded = false;
+        },
+        send() {
+            var fdata = new FormData();
+            var shouldSend = false;
+
+            if (this.preview) {
+                var file = $('input[type="file"]')[0].files[0];
+                if (file) {
+                    shouldSend = true;
+                    fdata.append("image", file);
+                }
+            }
+
+            if (this.newMessage.trim()) {
+                shouldSend = true;
+                fdata.append("content", this.newMessage);
+            }
+
+            if (shouldSend) {
+                fdata.append("conversation_id", this.conversation.id);
+                fdata.append("prio_auto", this.isATicket ? 1 : 0);
+                this.$emit('sendMessage', {fdata: fdata, isATicket: this.isATicket} );
+            }
+            this.newMessage = "";
+            this.resetFileInput();
+        },
+    },
+    watch: {
+        newMessage() {
+            if (
+                (this.newMessage.trim() && !this.imWriting) ||
+                (!this.newMessage.trim() && this.imWriting)
+            ) {
+                this.imWriting = !this.imWriting;
+                this.$emit('writingChange', this.imWriting);
+            }
+        }
+    }
+};
+
 </script>
 
 <style lang="scss" scoped>
+.form-container {
+    background: #e4e4e4;
+    border-top: solid 1px #e6e6e6;
+    outline: none;
+    z-index: 2;
+    box-shadow: 0 -2px 4px 0px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+.message-form {
+    padding: 0 8px;
+    width: 100%;
+    align-items: center;
+}
+.message-form input {
+    margin: 8px 0;
+    flex-grow: 1;
+    // height: px;
+    border-radius: 19px;
+    border: 0;
+    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.25);
+}
+.message-form input:focus {
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.25);
+}
+
+.message-form button,
+.adj-btn {
+    border: none;
+    background: transparent;
+    padding: 0;
+    outline: none;
+    // height: 38px;
+    color: #6fcea3;
+    margin: 0;
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
+    transition: box-shadow 0.15s ease-in-out;
+    cursor: pointer;
+}
+.message-form button:disabled {
+    color: #cccccc !important;
+}
+.message-form button:hover,
+.adj-btn {
+    color: #5db68e;
+}
+.message-form button:active {
+    color: #36a774;
+    text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.25);
+}
+
+.message-form button i,
+.adj-btn i {
+    font-size: 32px;
+}
 
 </style>

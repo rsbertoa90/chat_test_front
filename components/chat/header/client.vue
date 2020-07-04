a80ed2a26172b393a928f5a7cf42b3313b52bece
Juan Pablo Aveggio <juan@pop-os.localdomain>
2020-07-03 21:04:46 -0300

    cascara

--- components/chat/header/client.vue
+++ components/chat/header/client.vue
@@ -1,5 +1,9 @@
 <template>
-    
+    <div class="d-flex align-items-center header">
+        <i class="material-icons ml-5">textsms</i>
+        <span class="flex-grow-1 ml-2">Estas hablando con Mates Fabi</span>
+        <button class="d-flex mr-3 p-2 mat"><i class="material-icons">search</i></button>
+    </div>
 </template>
 
 <script>
@@ -9,5 +13,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.header {
+    background: #E45BA5;
+    height: 64px;
+    i {
+        //margin-left: 78px;
+        font-size: 24px;
+        color: #FFF;
+    }
+    span {
+        font-size: 16px;
+        color: #FFF;
+    }
+    button {
+        background: #FFF;
+        border-radius: 50%;
+        i {
+            color: rgba(0, 0, 0, 0.75);
+        }
+    }
+}
 </style>
